db072bd79bcb7851bd7d9240c78f7a01dc159e3d
Alexander Kleemann <kleemannalexander@gmail.com>
2019-04-12 17:08:48 +0200

    Add default values

--- armsdk/armory/blender/arm/logicnode/ppm_value_lerp_float.py
+++ armsdk/armory/blender/arm/logicnode/ppm_value_lerp_float.py
@@ -12,9 +12,13 @@ class LerpFloatNode(Node, ArmLogicTreeNode):
     def init(self, context):
         self.inputs.new('ArmNodeSocketAction', 'In')
         self.inputs.new('NodeSocketFloat', 'Starting Float')
+        self.inputs[-1].default_value = 0.0
         self.inputs.new('NodeSocketFloat', 'End Float')
+        self.inputs[-1].default_value = 1.0
         self.inputs.new('NodeSocketFloat', 'Duration')
+        self.inputs[-1].default_value = 2.0
         self.inputs.new('NodeSocketBool', 'Stop Interpolation')
         self.outputs.new('NodeSocketFloat', 'Float')
+        self.outputs.new('NodeSocketBool', 'Running')
         
 add_node(LerpFloatNode, category='PPM+')
